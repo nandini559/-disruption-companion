3e7a1dc59338279292381f6daec67b1e0fb8b09e
Nandini Tarafdar <nandinitarafdar481@gmail.com>
2026-09-04 15:45:52 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-50">
+      <h1 className="text-4xl font-bold  text-red-500">
+        Disruption Companion
+      </h1>
+    </div>
+  );
+}
+
+export default App;
